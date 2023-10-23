e5e4abd57d361561b88a78a9f4ee832230cc283e
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-10-23 15:27:47 +0530

    ok

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,7 +15,7 @@ class WheaterAppHome extends StatefulWidget {
 }
 
 class _WheaterAppHomeState extends State<WheaterAppHome> {
-
+  final controller = Get.put(DataController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,51 +31,48 @@ class _WheaterAppHomeState extends State<WheaterAppHome> {
           alignment: Alignment(0, 0),
           child: Container(
             height: 500,
-      child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              height: 300,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-
-                  Weather(),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: IconButton(
-                      icon: new Icon(Icons.refresh),
-                      tooltip: 'Refresh',
-                      onPressed: () => null,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              height: 200,
-              child: ListView.builder(
-                itemCount: 10,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          WeatherItem()
-                        ],
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 300,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Weather(),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: IconButton(
+                          icon: new Icon(Icons.refresh),
+                          tooltip: 'Refresh',
+                          onPressed: () => controller.getData(),
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
-      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 200,
+                  child: ListView.builder(
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[WeatherItem()],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
